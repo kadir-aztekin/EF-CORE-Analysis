29aebd8000c392903d1b12ed84a153dd49bf1b17
kadir-aztekin <aztekadir@icloud.com>
2022-09-23 00:40:19 +0300

    PMCDbFirstButSomeTableAndPath

--- PMC-DbFirst-ButSomeTable/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-//Scaffold - DbContext 'Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=True;'  Microsoft.EntityFrameworkCore.SqlServer - Tables Product,Orders,Customer,Employee
