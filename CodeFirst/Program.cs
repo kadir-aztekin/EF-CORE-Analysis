dd344b75351def9c656655f280249d0166dce6e0
kadir-aztekin <aztekadir@icloud.com>
2022-09-23 01:19:26 +0300

    CodeFirst and Migration, Path,getMigration ,downmig

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+
+//MİGRATİON CODE 
+//ECommerceDbContext context = new();
+//context.Database.Migrate();
+
+public class ECommerceDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ECommerDb;Trusted_Connection=True;");
+    }
+}
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public float Price { get; set; }
+}
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
--- DbFirstButSomeTableAndPath/Program.cs
+++ DbFirstButSomeTableAndPath/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//Scaffold - DbContext 'Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=True;' Microsoft.EntityFrameworkCore.SqlServer - Tables Products,Orders,Customers,Users - ContextDir Contexts - OutputDir Entites
