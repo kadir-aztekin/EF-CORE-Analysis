3cbb2673fc34f59a852fbaa33c65f649672e6741
kadir-aztekin <aztekadir@icloud.com>
2022-09-28 23:09:41 +0300

    ShadowProperty

---
+++ Shadow_Property/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+ApplicationDbContext context = new();
+
+
+Console.WriteLine("Hello, World!");
+#region Shadow Properties-Gölge
+//Entity sınıflarında fiziksel olarak tanımlanmayan/modellenmeyen ancak EF Core tarafından ilgili entity için var olan/var olduğu kabul edilen property'lerdir.
+//Tabloda gösterilmesini istemediğimiz entity instance'ı üzerinde işlem yapmayacağımız kolonlar için shadow propertyler kullanılabilir.
+//Shadow property'lerin değerleri ve stateleri Change Tracker tarafından kontrol edilir.
+#endregion
+
+#region Foreign Key -Shadow Properties
+//İlişkisel senaryolarda foreign key property'sini tanımlamadığımız halde EF Core tarafından dependent entity'e eklenmektedir. İşte bu shadow property'dir.
+var blogs = await context.Blogs.Include(b => b.Posts)
+    .ToListAsync();
+#endregion
+
+#region Shadow Property Oluşturma
+//Bir entity üzerinde shdow prop olustrmak ıstıyorsak Fluent APı kullanılır
+//protected override void OnModelCreating(ModelBuilder modelBuilder)
+//{
+//    modelBuilder.Entity<Blog>()
+//          .Property<DateTime>("CreatedDate");
+//}
+#endregion
+
+#region Shadow Property Erişim 
+#region ChanceTracker İle Erişim
+var blog = await context.Blogs.FindAsync();
+var createdDate = context.Entry(blog).Property("CreatedDate");
+Console.WriteLine(createdDate.CurrentValue);
+#endregion
+#region Ef.Property İle Erişim
+//Özellikle LINQ sorgularında Shadow Propery'lerine erişim için EF.Property static yapılanmasını kullanabiliriz.
+//var blogs = await context.Blogs.OrderBy(b => EF.Property<DateTime>(b, "CreatedDate")).ToListAsync();
+
+//var blogs2 = await context.Blogs.Where(b => EF.Property<DateTime>(b, "CreatedDate").Year > 2020).ToListAsync();
+//Console.WriteLine();
+#endregion
+#endregion
+
+
+
+
+
+
+
+class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Post> Posts { get; set; }
+}
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool lastUpdated { get; set; }
+
+    public Blog Blog { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ApplicationDb;Trusted_Connection=True;");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>()
+              .Property<DateTime>("CreatedDate");
+    }
+}
